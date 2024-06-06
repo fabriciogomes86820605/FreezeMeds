b4bcf0806343bcc5ab7b19c8a45275734498b089
DiegoDueranGarcia <diego.garcia@sptech.school>
2024-06-06 15:49:06 -0300

    Alteração do CSS da Dashboard e adição de uma tela semi-funcional para abertura de chamados de manutenção

--- FreezeMeds - Projeto/src/models/medidaModel.js
+++ FreezeMeds - Projeto/src/models/medidaModel.js
@@ -1,28 +1,29 @@
+
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idDados, limite_linhas) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+    temperatura, 
+        
+    registroData,
+                  DATE_FORMAT(momento,'%H:%i:%s') as MomentoAtual
                     FROM medida
-                    WHERE fk_aquario = ${idAquario}
+                    WHERE fkSensor = ${idDados}
                     ORDER BY id DESC LIMIT ${limite_linhas}`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(idDados) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
+    temperatura, 
+    registroData,
+                        DATE_FORMAT(momento,'%H:%i:%s') as MomentoAtual, 
+                        fkSensor 
+                        FROM medida WHERE fkSensor = ${idDados} 
                     ORDER BY id DESC LIMIT 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
